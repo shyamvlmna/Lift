2830c9af3c2490265707961706940181f059618b
shyamvlmna <shyamvlmna@gmail.com>
2022-06-19 16:48:04 +0530

    added otp validation added Redis

--- api/pkg/routes/userRoutes.go
+++ api/pkg/routes/userRoutes.go
@@ -11,13 +11,16 @@ func UserRoutes(r *mux.Router) {
 	userRouter := r.PathPrefix("/user").Subrouter()
 
 	userRouter.HandleFunc("/auth", controllers.UserAuth).Methods("POST")
+	userRouter.HandleFunc("/otp", controllers.ValidateOtp).Methods("POST")
+
+	userRouter.HandleFunc("/signup", controllers.UserSignUp).Methods("GET")
 	userRouter.HandleFunc("/signup", controllers.UserSignUp).Methods("POST")
 	userRouter.HandleFunc("/login", controllers.UserLogin).Methods("POST")
 
 	userRouter.HandleFunc("/userhome", controllers.UserHome).Methods("GET")
 	userRouter.HandleFunc("/update/{id}", controllers.EditUserProfile).Methods("GET")
 	userRouter.HandleFunc("/update/{id}", controllers.UpdateUserProfile).Methods("POST")
-	userRouter.HandleFunc("/logout", controllers.UserLogin).Methods("GET")
+	userRouter.HandleFunc("/logout", controllers.UserLogout).Methods("GET")
 
 	userRouter.HandleFunc("/getusers", controllers.GetUsers).Methods("GET")
 }
